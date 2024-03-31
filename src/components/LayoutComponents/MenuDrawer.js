cb7c600e10750ea90eb9421e6174b7648ebd2923
Matheus Amaral <m.amaral8998@gmail.com>
2024-03-31 15:22:46 -0300

    EDU-005 - criação menu drawer para tela home, ajuste estrutura tela home

---
+++ src/components/LayoutComponents/MenuDrawer.js
@@ -0,0 +1,158 @@
+import React from 'react'
+import { styled, useTheme } from '@mui/material/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { 
+  Grid,
+  Avatar, 
+  Typography, 
+  List, 
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Box,
+  Drawer,
+  CssBaseline,
+  Toolbar,
+  Divider,
+  IconButton,
+  } from '@mui/material'
+import MuiAppBar from '@mui/material/AppBar';
+import { deepOrange } from '@mui/material/colors';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  backgroundColor: '#02040f',
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function MenuDrawer({ children }) {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Grid container>
+            <Avatar sx={{ bgcolor: deepOrange[500] }} variant="square">A</Avatar>
+            <Typography color="white" fontSize={24} style={{ marginLeft: 10 }}>EDU.BOT</Typography>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <Grid container marginTop={5}>
+          {children}
+        </Grid>
+      </Main>
+    </Box>
+  );
+}
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,35 +1,118 @@
-import { Grid, Avatar, Typography } from '@mui/material'
-import React from 'react'
-import { deepOrange } from '@mui/material/colors';
+import { Grid, Avatar, List, ListItem, ListItemAvatar, ListItemText, TextField } from '@mui/material'
+import React, {useState, useEffect} from 'react'
+import MenuDrawer from '../../components/LayoutComponents/MenuDrawer';
 
 const Home = () => {
+  const [messages, setMessages] = useState([]);
+  const [mensagemDigitada, setMensagemDigitada] = useState("");
+
+  const getMessages = () => {
+    const mensagens = [
+      {
+        "id": 1,
+        "nomeUsuario": "Matheus Amaral da silva",
+        "descricao": "Um texto de testezinho"
+      },
+      {
+        "id": 2,
+        "nomeUsuario": "Matheus Amaral",
+        "descricao": "Um texto de testezinho"
+      },
+      {
+        "id": 3,
+        "nomeUsuario": "Matheus Amaral",
+        "descricao": "Um texto de testezinho"
+      },
+      {
+        "id": 4,
+        "nomeUsuario": "Matheus Amaral",
+        "descricao": "Um texto de testezinho"
+      },
+      {
+        "id": 5,
+        "nomeUsuario": "Matheus Amaral",
+        "descricao": "Um texto de testezinho"
+      },
+      {
+        "id": 6,
+        "nomeUsuario": "Matheus Amaral",
+        "descricao": "Um texto de testezinho"
+      },
+      {
+        "id": 7,
+        "nomeUsuario": "Matheus Amaral",
+        "descricao": "Um texto de testezinho"
+      },
+      {
+        "id": 8,
+        "nomeUsuario": "Matheus Amaral",
+        "descricao": "Um texto de testezinho"
+      },
+      {
+        "id": 9,
+        "nomeUsuario": "Matheus Amaral",
+        "descricao": "Um texto de testezinho"
+      },
+      {
+        "id": 9,
+        "nomeUsuario": "Matheus Amaral",
+        "descricao": "Um texto de testezinho"
+      },
+      {
+        "id": 9,
+        "nomeUsuario": "Matheus Amaral",
+        "descricao": "Um texto de testezinho"
+      },
+    ]
+    setMessages(mensagens)
+  }
+
+  const isSentByCurrentUser = (user) => {
+    return user === "Matheus Amaral"
+  }
+
+  useEffect(() => {
+    getMessages();
+  }, [])
   return (
-    <Grid container style={{ height: '100vh', width: '100%' }}>
-      <Grid item xs={2.5} style={{ backgroundColor: '#02040f', display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
-        Lado
-
-      </Grid>
-      <Grid item xs={9.5} style={{ backgroundColor: '#f8f8ff', display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
-        <Grid container style={{ height: "100%" }}>
-          <Grid item xs={12} style={{ backgroundColor: "#02040f", display: 'flex', height: "12%" }}>
-            <Grid>
-              <Avatar sx={{ bgcolor: deepOrange[500] }} variant="square">
-                A
-              </Avatar>
-              <Typography color="white" fontSize={24}>
-                EDU.BOT
-              </Typography>
-            </Grid>
+    <MenuDrawer>
+      <Grid container>
+          <Grid item xs={12} style={{flexGrow: 1, overflow: "auto", height: "80vh" }}>
+            <List style={{ display: 'flex', flexDirection: 'column' }}>
+              {messages.map((message, index) => (
+                <ListItem 
+                  key={index} 
+                  style={{ 
+                    textAlign: isSentByCurrentUser(message.nomeUsuario) ? 'right' : 'left',
+                    flexDirection: isSentByCurrentUser(message.nomeUsuario) ? 'row-reverse' : 'row'
+                  }}
+                >
+                  <ListItemAvatar style={{ display: 'flex', 
+                    justifyContent: isSentByCurrentUser(message.nomeUsuario) ? 'flex-end' : 'flex-start' }}>
+                    <Avatar alt="Profile Picture" />
+                  </ListItemAvatar>
+                  <ListItemText 
+                    primary={message.nomeUsuario} 
+                    secondary={message.descricao}
+                    style={{ textAlign: isSentByCurrentUser(message.nomeUsuario) ? 'right' : 'left' }}
+                  />
+                </ListItem>
+              ))}
+            </List>
           </Grid>
-          <Grid item xs={12} style={{ backgroundColor: "#fff", display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            baixo
-
+          <Grid item xs={12}> 
+            <TextField
+                  label="Digite sua mensagem"
+                  variant="outlined"
+                  fullWidth
+                  value={mensagemDigitada}
+                  onChange={(e) => {
+                    setMensagemDigitada(e.target.value);
+                  }}
+                />
           </Grid>
-        </Grid>
-
       </Grid>
-
-    </Grid>
+    </MenuDrawer>
   )
 }
 
