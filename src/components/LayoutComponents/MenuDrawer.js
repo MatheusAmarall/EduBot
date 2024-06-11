e3016cc6d4b77f54496f78dc537f163b7fc70b04
Matheus Amaral <m.amaral8998@gmail.com>
2024-06-11 13:06:38 -0300

    EDU-026 - resolvendo erro na troca de conversa

--- src/components/LayoutComponents/MenuDrawer.js
+++ src/components/LayoutComponents/MenuDrawer.js
@@ -184,6 +184,7 @@ export default function MenuDrawer({ children }) {
       }
     }
     globalContext.selecionaConversaUsuario(nomeUsuario)
+    navigate("/home")
   }
 
   const handleConcluirAtendimento = async () => {
